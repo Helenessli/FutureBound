faa8cd5cd638a810a53ff335e92921daef7ba376
helenessli <helenessli6@gmail.com>
2023-07-18 14:34:38 -0400

    added tables in migrations

--- database/migrations/2014_10_12_000000_create_scholarships_table.php
+++ database/migrations/2014_10_12_000000_create_scholarships_table.php
@@ -11,14 +11,15 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('scholarships', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->string('amount');
+            $table->text('criteria');
+            $table->date('deadline');
+            $table->string('url');
+            $table->date('created at');
+            $table->date('updated at');
         });
     }
 
@@ -27,6 +28,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('scholarships');
     }
 };
